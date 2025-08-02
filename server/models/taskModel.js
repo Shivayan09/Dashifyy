f91bca37ae3948382752f7f6b5eeb8db66dc64a6
Shivayan Chakravarty <your_email@example.com>
2025-08-03 01:31:04 +0530

    task schema

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -1,2 +1,20 @@
 import mongoose from "mongoose";
 
+const taskShema = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true
+    },
+    isDone: {
+        type: Boolean,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    },
+})
+
+const taskModel = mongoose.model.subject || mongoose.model('task', taskShema)
+
+export default taskModel
