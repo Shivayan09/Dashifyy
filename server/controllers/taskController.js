8f743142bb1be3f19cf9ada612686df94c6d581d
Shivayan Chakravarty <shivayanchak@gmail.com>
2025-08-03 01:39:46 +0530

    addTodo

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,3 +1,31 @@
+import taskModel from "../models/taskModel"
+
 export const addTask = async(req, res) => {
-    
+    const {task} = req.body
+    if(!task) {
+        return res.json({
+            success: false,
+            message: "Task name is required"
+        })
+    }
+    try {
+        const existingTask = await taskModel.findOne({task})
+        if(existingTask) {
+            return res.json({
+                success: false,
+                message: "Task already exists!"
+            })
+        }
+        const newTask = new taskModel({task})
+        await newTask.save();
+        return res.status(200).json({
+            success: true,
+            message: "Task added successfully!"
+        })
+    } catch(error) {
+        return res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
 }
