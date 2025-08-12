1612199580142e01714e3d69c983b22db5cd855b
Shivayan Chakravarty <shivayanchak@gmail.com>
2025-08-12 23:43:54 +0530

    responsiveness for contact page

--- client/src/pages/Dashboard/Contact.jsx
+++ client/src/pages/Dashboard/Contact.jsx
@@ -5,8 +5,8 @@ import contact_us_icon3 from '../../assets/contactUs-icon3.png'
 
 const Contact = () => {
   return (
-    <div className='flex'>
-      <div className="box w-[43%] h-screen py-10 px-10">
+    <div className='flex flex-col-reverse md:flex-row'>
+      <div className="box w-[100%] md:w-[43%] h-screen py-10 px-10">
         <p className='text-purple-800/80 text-[2rem] font-bold'>Contact us</p>
         <p className='text-purple-900/70 my-6'>Send us an email and we'll get right back to you</p>
         <div className="input-box flex flex-col gap-3">
@@ -18,8 +18,8 @@ const Contact = () => {
           Send email
         </button>
       </div>
-      <div className="box w-[57%] h-screen flex items-center justify-center">
-        <img src={contact_us_icon3} alt="" className='opacity-80 h-[70vh]' />
+      <div className="box w-[100%] md:w-[57%] h-[40vh] md:h-screen flex items-center justify-center">
+        <img src={contact_us_icon3} alt="" className='opacity-80 h-[40vh] md:h-[70vh]' />
       </div>
     </div>
   )
